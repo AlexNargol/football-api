4a38cfc5413b91b16016cfc29369ce2e97346de0
AlexN <alex.nargol@live.co.uk>
2023-02-14 22:22:21 +0000

    Updated server to return an example json file

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
+import json
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/api/hello')
 def hello():
-    response = {'message': 'Hello, World!'}
-    return jsonify(response)
+    with open('example_json.json', 'r') as f:
+        data = json.load(f)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
